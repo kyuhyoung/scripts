7df07ad31b9a125761757d2895dc0b1e3a05656f
kyuhyoung <kyuhyoung@gmail.com>
2018-12-04 15:47:44 +0900

    Create init_ubuntu.sh

---
+++ init_ubuntu.sh
@@ -0,0 +1,15 @@
+sudo apt-get -y remove tmux
+sudo apt-get -y install wget tar libevent-dev libncurses-dev
+cd ~/Downloads
+VERSION=2.6
+wget https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
+tar xf tmux-${VERSION}.tar.gz
+rm -f tmux-${VERSION}.tar.gz
+cd tmux-${VERSION}
+./configure
+make
+sudo make install
+cd -
+sudo rm -rf /usr/local/src/tmux-\*
+sudo mv tmux-${VERSION} /usr/local/src
+sudo killall -9 tmux
