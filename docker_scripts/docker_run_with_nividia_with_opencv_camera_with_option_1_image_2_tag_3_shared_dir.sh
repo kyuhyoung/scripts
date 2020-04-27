e8bec6d67ad63283e062772af957a0a3cc316f38
kyuhyoung <kyuhyoung@gmail.com>
2020-04-27 20:04:49 +0900

    Update docker_run_with_nividia_with_opencv_camera_with_option_1_image_2_tag_3_shared_dir.sh

--- docker_scripts/docker_run_with_nividia_with_opencv_camera_with_option_1_image_2_tag_3_shared_dir.sh
+++ docker_scripts/docker_run_with_nividia_with_opencv_camera_with_option_1_image_2_tag_3_shared_dir.sh
@@ -8,7 +8,6 @@
 # visionking/unix:0  MIT-MAGIC-COOKIE-1  2150216277d8307bb93d409800ad18a4       
 # visionking/unix:10  MIT-MAGIC-COOKIE-1  c64df63afbc9650fac8840aaf9a1c684   
 # visionking/unix:11  MIT-MAGIC-COOKIE-1  ab8cc4949d7e4ea7c3d383d5ad398b67      
-# Copy "visionking/unix:0  MIT-MAGIC-COOKIE-1  2150216277d8307bb93d409800ad18a4"
 # Create container by  
 # $ sh docker_with_nividia_run_with_option_1_image_2_tag_3_shared_dir mmdetection latest ~/data
 # This will call
@@ -18,7 +17,12 @@ sudo docker run --gpus all -ti --shm-size 8G --rm --net=host -e QT_X11_NO_MITSHM
 # In container,
 # $ apt-get update
 # Install xauth
-# $ apt-get xauth
-# Add the copied such as following.
-# $ xauth add visionking/unix:0  MIT-MAGIC-COOKIE-1  2150216277d8307bb93d409800ad18a4
+# $ apt-get install xauth
+# Add the results of "xauth list" as following.
+# $ xauth add visionking/unix:12  MIT-MAGIC-COOKIE-1  d04060f052faab8a805637da30dbbd61
+# $ xauth add visionking/unix:13  MIT-MAGIC-COOKIE-1  b4b596ec1d49771b5262ebdb9b0e6599
+# $ xauth add visionking/unix:14  MIT-MAGIC-COOKIE-1  81fb5fbefa0fc8ffec8e7379204895be          
+# $ xauth add visionking/unix:0  MIT-MAGIC-COOKIE-1  2150216277d8307bb93d409800ad18a4       
+# $ xauth add visionking/unix:10  MIT-MAGIC-COOKIE-1  c64df63afbc9650fac8840aaf9a1c684   
+# $ xauth add visionking/unix:11  MIT-MAGIC-COOKIE-1  ab8cc4949d7e4ea7c3d383d5ad398b67
 # Run opencv application using webcam.
