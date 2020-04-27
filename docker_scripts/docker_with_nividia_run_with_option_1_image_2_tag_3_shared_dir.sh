d32e73e5777261b3193a4b3b161a6399e91f4cd1
kyuhyoung <kyuhyoung@gmail.com>
2020-04-27 18:00:12 +0900

    Update docker_with_nividia_run_with_option_1_image_2_tag_3_shared_dir.sh

--- docker_scripts/docker_with_nividia_run_with_option_1_image_2_tag_3_shared_dir.sh
+++ docker_scripts/docker_with_nividia_run_with_option_1_image_2_tag_3_shared_dir.sh
@@ -4,4 +4,4 @@
 # will call
 # sudo docker run --gpus all -ti --shm-size 8G --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix. -v ~/data/:/mnt/data  mmdetection:latest
 dir_shared="$(basename "$3")"
-sudo docker run --gpus all -ti --shm-size 8G --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix. -v $3:/mnt/$dir_shared $1:$2
+sudo docker run --gpus all -ti --shm-size 8G --rm -e DISPLAY=$DISPLAY -e="QT_X11_NO_MITSHM=1" -v /dev/video0:/dev/video0 -v /tmp/.X11-unix:/tmp/.X11-unix:ro -v $3:/mnt/$dir_shared $1:$2
