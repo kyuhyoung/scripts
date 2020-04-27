a04f91076f272938e80fc5f22fd3e3e497545e3f
kyuhyoung <kyuhyoung@gmail.com>
2020-04-27 14:06:52 +0900

    Create docker_with_nividia_run_with_option_1_image_2_tag_3_shared_dir.sh

---
+++ docker_with_nividia_run_with_option_1_image_2_tag_3_shared_dir.sh
@@ -0,0 +1,6 @@
+# For example,
+# sh docker_with_nividia_run_with_option_1_image_2_tag_3_shared_dir mmdetection latest ~/data
+# will call
+# sudo docker run --gpus all -ti --shm-size 8G --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix. -v ~/data/:/mnt/data  mmdetection:latest
+dir_shared="$(basename "$3")"
+sudo docker run --gpus all -ti --shm-size 8G --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix. -v $3:/mnt/$(dir_shared) $1:$2
