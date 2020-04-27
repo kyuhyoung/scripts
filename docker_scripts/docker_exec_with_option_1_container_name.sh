8a68fd8f0ae1ef9150202f0d982e0321380ec664
kyuhyoung <kyuhyoung@gmail.com>
2020-04-27 14:21:50 +0900

    Update docker_exec_with_option_1_container_name.sh

--- docker_scripts/docker_exec_with_option_1_container_name.sh
+++ docker_scripts/docker_exec_with_option_1_container_name.sh
@@ -1,1 +1,5 @@
-...
+# For example,
+# sh docker_exec_with_option_1_container_name.sh kind_goldwasser
+# will call
+#sudo docker exec -it --user root --workdir / kind_goldwasser /bin/bash
+sudo docker exec -it --user root --workdir / $1 /bin/bash
