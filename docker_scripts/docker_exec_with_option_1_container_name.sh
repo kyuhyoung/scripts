47735bb59b474f574c96ffdb7520b62aebb01b36
kyuhyoung <kyuhyoung@gmail.com>
2020-04-28 12:26:55 +0900

    Update docker_exec_with_option_1_container_name.sh

--- docker_scripts/docker_exec_with_option_1_container_name.sh
+++ docker_scripts/docker_exec_with_option_1_container_name.sh
@@ -1,5 +1,6 @@
 # For example,
 # sh docker_exec_with_option_1_container_name.sh kind_goldwasser
 # will call
-#sudo docker exec -it --user root --workdir / kind_goldwasser /bin/bash
+# sudo docker exec -it --user root --workdir / kind_goldwasser /bin/bash
+# Here "--user root --workdir /" will put you start at the "/" directory.
 sudo docker exec -it --user root --workdir / $1 /bin/bash
