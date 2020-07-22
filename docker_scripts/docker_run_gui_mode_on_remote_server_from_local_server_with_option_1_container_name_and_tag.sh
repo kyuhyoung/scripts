138ae1aca437744dab7c20745a5d9edb4948d2f9
kyuhyoung <kyuhyoung@gmail.com>
2020-07-22 21:11:07 +0900

    Update docker_run_gui_mode_on_remote_server_from_local_server_with_option_1_container_name_and_tag.sh

--- docker_scripts/docker_run_gui_mode_on_remote_server_from_local_server_with_option_1_container_name_and_tag.sh
+++ docker_scripts/docker_run_gui_mode_on_remote_server_from_local_server_with_option_1_container_name_and_tag.sh
@@ -1,3 +1,6 @@
+# ssh 접속
+# gedit
+# 만약 gedit 창이 remote server에서 보여지면 아래를 실행하고 그렇지 않으면 보여질 때 까지 작업한다.
 XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth-n
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
