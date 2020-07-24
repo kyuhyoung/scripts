bc6a26d99f1839efdf15696bcc757aa0e8d36b49
kyuhyoung <kyuhyoung@gmail.com>
2020-07-24 17:54:14 +0900

    Update docker_run_gui_mode_on_remote_server_from_local_server_with_option_1_container_name_and_tag.sh

--- docker_scripts/docker_run_gui_mode_on_remote_server_from_local_server_with_option_1_container_name_and_tag.sh
+++ docker_scripts/docker_run_gui_mode_on_remote_server_from_local_server_with_option_1_container_name_and_tag.sh
@@ -5,4 +5,4 @@ XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth-n
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 chmod 777 $XAUTH
-docker run -ti -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH --net host $1 /bin/bash
+docker run -ti -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH --net host $1 /bin/bash -c "apt-get update; apt-get install -y fish"
