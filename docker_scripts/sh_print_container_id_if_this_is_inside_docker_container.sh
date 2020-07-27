ae05e183b1d836a2c930d369224ad13a8703122f
kyuhyoung <kyuhyoung@gmail.com>
2020-07-27 14:41:02 +0900

    Update sh_print_container_id_if_this_is_inside_docker_container.sh

--- docker_scripts/sh_print_container_id_if_this_is_inside_docker_container.sh
+++ docker_scripts/sh_print_container_id_if_this_is_inside_docker_container.sh
@@ -1,4 +1,4 @@
 # Check if this is inside a docker containier
 # If so, the container ID such as "8b89729774cceb78289ef8a5ce1321d81bb29ff25c1c3e1840ff29d49c3564f6" would be printed.
 # Otherwise, nothing will be printed.
-awk -F/ '$2 == "docker"' /proc/self/cgroup | read | xargs basename
+awk -F/ '$2 == "docker"' /proc/self/cgroup | head -n 1 | xargs basename
