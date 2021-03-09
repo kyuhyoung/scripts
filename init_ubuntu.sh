822ddb8e7fd78bf6816d587f0e50ded556f3d90c
kyuhyoung <kyuhyoung@gmail.com>
2021-03-09 14:02:59 +0900

    Update init_ubuntu.sh

--- init_ubuntu.sh
+++ init_ubuntu.sh
@@ -31,7 +31,8 @@ sudo apt-get -y remove tmux
 sudo apt-get -y install wget tar libevent-dev libncurses-dev
 cd ~/Downloads
 VERSION=2.6
-curl -O https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
+#curl -O https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
+wget https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
 tar xf tmux-${VERSION}.tar.gz
 rm -f tmux-${VERSION}.tar.gz
 cd tmux-${VERSION}
