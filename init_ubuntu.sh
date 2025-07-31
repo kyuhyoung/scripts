0636812b5319670f9a4cab8185a2a3d03d4d8ab0
Kyuhyoung Choi <kyuhyoung@gmail.com>
2025-07-31 15:29:24 +0900

    Update init_ubuntu.sh

--- init_ubuntu.sh
+++ init_ubuntu.sh
@@ -11,6 +11,24 @@ if [ "$#" -ne 1 ] || ! [[ $1 == *"@"* ]]; then
   exit 1
 fi
 
+sudo rm -rf ~/work/ubuntu_init
+sudo mkdir -p ~/work/ubuntu_init
+
+echo ""
+echo "========================================================"
+echo "========  mosh ========================================="
+echo "========================================================"
+sudo apt-get install -y protobuf-compiler build-essential libncurses5-dev libncursesw5-dev libtinfo-dev pkg-config
+cd ~/work/ubuntu_init
+sudo wget https://github.com/mobile-shell/mosh/releases/download/mosh-1.4.0/mosh-1.4.0.tar.gz
+sudo tar xf mosh-1.4.0.tar.gz
+cd mosh-1.4.0
+sudo bash ./autogen.sh
+sudo bash ./configure
+sudo make
+sudo make install
+sudo make check
+
 echo ""
 echo "========================================================"
 echo "========  nvidia-docker 2 =============================="
@@ -26,29 +44,13 @@ sudo apt update
 sudo apt-get install -y nvidia-docker2
 sudo systemctl restart docker
 sudo docker run --rm --gpus all ubuntu:18.04 nvidia-smi
+sudo apt autoremove -y
 
 echo ""
 echo "========================================================"
 echo "========  apt-get packages ============================="
 echo "========================================================"
 sudo apt-get install -y wget gedit curl vim vim-gui-common vim-runtime fish git rename geeqie
-sudo rm -rf ~/work/ubuntu_init
-sudo mkdir -p ~/work/ubuntu_init
-
-# vim
-echo ""
-echo "========================================================"
-echo "========  vim =========================================="
-echo "========================================================"
-
-cd ~/work/ubuntu_init
-sudo rm -rf /root/.vim*
-sudo git clone https://github.com/kyuhyoung/vi_setting.git
-cd vi_setting
-sudo cp -r .vim* ~/
-sudo vi +'source ~/.vimrc' +qa
-sudo vi +'PlugInstall' +qa
-
 
 # fish
 echo ""
@@ -60,48 +62,6 @@ sudo curl -O https://raw.githubusercontent.com/kyuhyoung/scripts/master/export.f
 sudo mkdir -p ~/.config/fish/functions
 sudo mv export.fish ~/.config/fish/functions/
 
-# tmux 2.6
-echo ""
-echo "========================================================"
-echo "========  tmux ========================================="
-echo "========================================================"
-cd ~/work/ubuntu_init
-
-sudo apt install -y libevent-dev ncurses-dev build-essential
-sudo wget https://github.com/tmux/tmux/releases/download/3.4/tmux-3.4.tar.gz
-sudo tar -xzf tmux-3.4.tar.gz
-cd tmux-3.4
-sudo apt update && sudo apt install bison
-sudo ./configure && sudo make
-sudo make install
-
-sudo git clone https://github.com/kyuhyoung/tmux.git
-cd tmux
-sudo cp -r .tmux* ~/
-sudo chown $(whoami):$(whoami) -R ~/.tmux
-cd ~/.tmux/plugins
-sudo git clone https://github.com/tmux-plugins/tpm.git 
-sudo git clone https://github.com/NHDaly/tmux-better-mouse-mode.git
-sudo git clone https://github.com/tmux-plugins/tmux-continuum.git
-sudo git clone https://github.com/tmux-plugins/tmux-resurrect.git
-sudo git clone https://github.com/tmux-plugins/tmux-sensible.git
-tmux source-file ~/.tmux.conf
-
-#sudo apt-get -y remove tmux
-#sudo apt-get -y install wget tar libevent-dev libncurses-dev
-#cd ~/Downloads
-#VERSION=2.6
-#wget https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
-#tar xf tmux-${VERSION}.tar.gz
-#rm -f tmux-${VERSION}.tar.gz
-#cd tmux-${VERSION}
-#./configure
-#make
-#sudo make install
-#curl -O https://raw.githubusercontent.com/kyuhyoung/scripts/master/.tmux.2.1.later.conf
-#sudo mv .tmux.2.1.later.conf ~/.tmux.conf
-#tmux source-file ~/.tmux.conf
-
 echo ""
 echo "========================================================"
 echo "========  Google Chrome ================================"
@@ -110,7 +70,6 @@ cd ~/work/ubuntu_init
 sudo apt --fix-broken install
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
-
 
 # github, the last because of gedit.
 echo ""
@@ -124,7 +83,7 @@ git config --global user.name "Kyuhyoung Choi"
 ssh-keygen -t rsa -C $1
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
-gedit ~/.ssh/id_rsa.pub
+cat ~/.ssh/id_rsa.pub
 
 echo ""
 echo "========================================================"
@@ -137,3 +96,46 @@ sudo cp /etc/samba/smb.conf /etc/samba/smb.conf_temp
 echo -e "[${WHOAMI}]\n comment = directory of ${WHOAMI}\n path = /home/${WHOAMI}\n valid users = ${WHOAMI}\n writeable = yes\n read only = no\n create mode = 0777\n directory mode = 0777" | sudo tee -a /etc/samba/smb.conf
 sudo service smbd restart
 sudo chown -R ${WHOAMI}:${WHOAMI} /home/${WHOAMI}
+
+# tmux 3.4
+echo ""
+echo "========================================================"
+echo "========  tmux ========================================="
+echo "========================================================"
+cd ~/work/ubuntu_init
+sudo apt install -y libevent-dev ncurses-dev build-essential
+sudo wget https://github.com/tmux/tmux/releases/download/3.4/tmux-3.4.tar.gz
+sudo tar -xzf tmux-3.4.tar.gz
+cd tmux-3.4
+sudo apt update && sudo apt install bison
+sudo ./configure && sudo make
+sudo make install
+
+sudo rm -rf tmux
+sudo git clone https://github.com/kyuhyoung/tmux.git
+cd tmux
+sudo cp -r .tmux* ~/
+sudo chown $(whoami):$(whoami) -R ~/.tmux
+cd ~/.tmux/plugins
+sudo git clone https://github.com/tmux-plugins/tpm.git 
+sudo git clone https://github.com/NHDaly/tmux-better-mouse-mode.git
+sudo git clone https://github.com/tmux-plugins/tmux-continuum.git
+sudo git clone https://github.com/tmux-plugins/tmux-resurrect.git
+sudo git clone https://github.com/tmux-plugins/tmux-sensible.git
+tmux source-file ~/.tmux.conf
+
+# vim
+echo ""
+echo "========================================================"
+echo "========  vim =========================================="
+echo "========================================================"
+cd ~/work/ubuntu_init
+sudo rm -rf /root/.vim*
+sudo rm -rf vi_setting
+sudo git clone https://github.com/kyuhyoung/vi_setting.git
+cd vi_setting
+sudo cp -r .vim* ~/
+sudo curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sudo chown $USER: ~/.vim
+sudo vi +'source ~/.vimrc' +qa
+sudo vi +'PlugInstall' +qa
